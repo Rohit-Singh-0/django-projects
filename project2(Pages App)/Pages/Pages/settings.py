174f3f51d9d5def43917da8ba8bffb67458166a8
Rohit-Singh-0 <rohitsingh3640@gmail.com>
2023-07-16 13:34:02 +0530

    New updates for Heroku deployment

--- project2(Pages App)/Pages/Pages/settings.py
+++ project2(Pages App)/Pages/Pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*+iiu9kl#ue=(+t&@h*677)!6tf$nr90-&gu_l*yc&+_@=7b$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
